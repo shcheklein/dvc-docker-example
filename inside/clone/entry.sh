d591faa7ad2480f0ed9746f75ca3f683acf582d4
Ivan Shcheklein <shcheklein@gmail.com>
2023-06-14 15:49:29 -0700

    feat(inside): fix exp push command

--- inside/clone/entry.sh
+++ inside/clone/entry.sh
@@ -7,5 +7,5 @@ cd workdir/inside/clone
 git config --local user.name "Ivan Shcheklein"
 git config --local user.email "shcheklein@gmail.com"
 dvc exp run --pull --allow-missing --message 'Docker Experiment'
-dvc exp push origin HEAD
+dvc exp push origin --rev HEAD
 
